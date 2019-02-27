5e21aafa1f0ef5910986c74e6fd2c0a678c16ec9
Jude Kc <amozu2008@gmail.com>
2019-02-27 21:29:58 +0000

    Ok after add console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string DisplayMessage()
+        {
+            return "Hellow World";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DisplayMessage());
+        }
+    }
+}
